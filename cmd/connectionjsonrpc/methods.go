1b59e84e005133b1555d199921e6d1aa94dc618a
Артемий Беляков (Москва) <a.belyakov@cloud.gcm>
2025-10-29 11:53:42 +0300

    feat: Написать методы взаимодействия по JsonRPC 	modified:   cmd/connectionjsonrpc/helpermethods.go 	modified:   cmd/connectionjsonrpc/methods.go

--- cmd/connectionjsonrpc/methods.go
+++ cmd/connectionjsonrpc/methods.go
@@ -3,14 +3,16 @@ package connectionjsonrpc
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"strings"
 )
 
-// AuthorizationStart попытка получить хеш авторизации который необходим для
-// дальнейшей работы с API
+// AuthorizationStart авторизация клиента
+// В результате авторизации должен быть получен хеш авторизации который будет
+// добавлятся в каждый последующий запрос.
 func (api *ZabbixConnectionJsonRPC) AuthorizationStart(ctx context.Context) error {
 	data := strings.NewReader(fmt.Sprintf(`{
 	  "jsonrpc":"2.0",
@@ -56,63 +58,100 @@ func (api *ZabbixConnectionJsonRPC) GetAuthorizationData() string {
 	return api.authorizationHash
 }
 
-/*
-* Здесь надо подумать
+// ActionGet извлечение данных.
+// Содержимое 'param' должно являтся строковым представление JSON формата с определёнными
+// значениями. Подробнее о типах значений и их структуре можно узнать из официальной
+// документации https://www.zabbix.com/documentation/current/en/manual/api/reference/action/get.
+func (api *ZabbixConnectionJsonRPC) ActionGet(ctx context.Context, param string) ([]byte, error) {
+	return api.sendRequest(
+		ctx,
+		strings.NewReader(
+			fmt.Sprintf(`{
+	  			"jsonrpc":"2.0",
+	  			"method":"action.get",
+	  			"params": %s,
+	  			"id":1
+			}`, param)))
+}
 
-// Request запрос к API
+// ActionCreate добавления новых действий над уже имеющимися данными.
+// Например, новых триггеров, 'действий обнаружения', 'действий авторегистрации' и т.д.
+// Содержимое 'param' должно являтся строковым представление JSON формата с определёнными
+// значениями. Подробнее о типах значений и их структуре можно узнать из официальной
+// документации https://www.zabbix.com/documentation/current/en/manual/api/reference/action/create
+func (api *ZabbixConnectionJsonRPC) ActionCreate(ctx context.Context, param string) ([]byte, error) {
+	return api.sendRequest(
+		ctx,
+		strings.NewReader(
+			fmt.Sprintf(`{
+	  			"jsonrpc":"2.0",
+	  			"method":"action.create",
+	  			"params": %s,
+	  			"id":1
+			}`, param)))
+}
 
-	func (api *RequiestSensorInfo) Request(ctx context.Context, params string) (string, error) {
-		request := fmt.Sprintf(`{
-	      "jsonrpc":"2.0",
-		  "method":"item.get",
-		  "params":%s,
-		  "id":1
-		}`, params)
+// ActionUpdate обновления уже существующих тригеров, 'действий обнаружения', 'действий авторегистрации' и т.д.
+// Содержимое 'param' должно являтся строковым представление JSON формата с определёнными
+// значениями. Подробнее о типах значений и их структуре можно узнать из официальной
+// документации https://www.zabbix.com/documentation/current/en/manual/api/reference/action/update.
+func (api *ZabbixConnectionJsonRPC) ActionUpdate(ctx context.Context, param string) ([]byte, error) {
+	return api.sendRequest(
+		ctx,
+		strings.NewReader(
+			fmt.Sprintf(`{
+	  			"jsonrpc":"2.0",
+	  			"method":"action.update",
+	  			"params": %s,
+	  			"id":1
+			}`, param)))
+}
 
-		return api.sendRequest(ctx, request)
+// ActionDelete удаление действий установленных ранее.
+// Подробнее о типах значений и их структуре можно узнать из официальной
+// документации https://www.zabbix.com/documentation/current/en/manual/api/reference/action/delete.
+// В сигнатуре функции перечень id является идентификатор ранее установленных тригеров
+func (api *ZabbixConnectionJsonRPC) ActionDelete(ctx context.Context, id ...string) ([]byte, error) {
+	if len(id) == 0 {
+		return nil, errors.New("deletion cannot be performed, the list of IDs must not be empty")
 	}
 
-// sendRequest передача запроса к API
+	res, err := api.sendRequest(
+		ctx,
+		strings.NewReader(
+			fmt.Sprintf(`{
+	  			"jsonrpc":"2.0",
+	  			"method":"action.delete",
+	  			"params":[%s],
+	  			"id":1
+			}`, strings.Join(id, ","))))
 
-	func (api *RequiestSensorInfo) sendRequest(ctx context.Context, str string) (string, error) {
-		res, err := api.zabbixConnection.postRequest(ctx, strings.NewReader(str))
+	return res, err
+}
+
+// sendRequest обрабатывает запрос
+func (api *ZabbixConnectionJsonRPC) sendRequest(ctx context.Context, r *strings.Reader) ([]byte, error) {
+	response, err := api.postRequest(ctx, r)
+	if err != nil {
+		// при возникновении ошибки пытаемся авторизоватся повторно,
+		// так как при устаревании авторизационного хеша возможно появления
+		// ошибки с сообщением:
+		// 'Invalid params. Session terminated, re-login, please.'
+		err = api.AuthorizationStart(ctx)
 		if err != nil {
-			return "", err
+			return nil, err
 		}
 
-		var resData ResponseData
-		err = json.Unmarshal(res, &resData)
+		response, err = api.postRequest(ctx, r)
 		if err != nil {
-			return "", err
+			return nil, err
 		}
-
-		if len(resData.Error) > 0 {
-			var msg, data string
-
-			for k, v := range resData.Error {
-				if k == "message" {
-					msg = fmt.Sprint(v)
-				}
-
-				if k == "data" {
-					data = fmt.Sprint(v)
-				}
-			}
-
-			return "", fmt.Errorf("%s. %s", msg, data)
-		}
-
-		for _, v := range resData.Result {
-			for key, value := range v {
-				if key == "lastvalue" {
-					return fmt.Sprint(value), nil
-				}
-			}
-		}
-
-		return "", nil
 	}
-*/
+
+	return response, err
+}
+
+// postRequest выполняет POST запрос
 func (api *ZabbixConnectionJsonRPC) postRequest(ctx context.Context, data *strings.Reader) ([]byte, error) {
 	req, err := http.NewRequestWithContext(ctx, "POST", api.url, data)
 	if err != nil {
