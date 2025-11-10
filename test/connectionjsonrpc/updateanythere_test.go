5148a769ecf5b1e411d40c4d5c558f7e20969d74
Артемий Беляков (Москва) <a.belyakov@cloud.gcm>
2025-11-10 17:52:00 +0300

    test: Написать тесты обновления хоста и группы хостов 	new file:   test/connectionjsonrpc/updateanythere_test.go

---
+++ test/connectionjsonrpc/updateanythere_test.go
@@ -0,0 +1,86 @@
+package connectionjsonrpc_test
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/subosito/gotenv"
+
+	connjsonrpc "github.com/av-belyakov/zabbixapicommunicator/v2/cmd/connectionjsonrpc"
+)
+
+func TestUpdateAnyThere(t *testing.T) {
+	var (
+		zc *connjsonrpc.ZabbixConnectionJsonRPC
+
+		err error
+	)
+
+	if err := gotenv.Load(".env"); err != nil {
+		log.Fatalln(err)
+	}
+
+	zHost := os.Getenv("GO_TESTZABBIX_HOST")
+	if zHost == "" {
+		log.Fatalln("environment variable 'GO_TESTZABBIX_HOST' cannot be empty")
+	}
+
+	zPort := os.Getenv("GO_TESTZABBIX_PORT")
+	if zPort == "" {
+		log.Fatalln("environment variable 'GO_TESTZABBIX_PORT' cannot be empty")
+	}
+
+	zUser := os.Getenv("GO_TESTZABBIX_USER")
+	if zUser == "" {
+		log.Fatalln("environment variable 'GO_TESTZABBIX_USER' cannot be empty")
+	}
+
+	zPasswd := os.Getenv("GO_TESTZABBIX_PASSWD")
+	if zPasswd == "" {
+		log.Fatalln("environment variable 'GO_TESTZABBIX_PASSWD' cannot be empty")
+	}
+
+	port, err := strconv.Atoi(zPort)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	t.Run("Тест 0. Инициализация соединения и получение авторизационного токена", func(t *testing.T) {
+		zc, err = connjsonrpc.NewConnect(
+			connjsonrpc.WithPort(port),
+			connjsonrpc.WithHost(zHost),
+			connjsonrpc.WithConnectionTimeout(30),
+			connjsonrpc.WithLogin(zUser),
+			connjsonrpc.WithPasswd(zPasswd),
+		)
+		assert.NoError(t, err)
+
+		err = zc.AuthorizationStart(t.Context())
+		assert.NoError(t, err)
+	})
+
+	t.Run("Тест 1. Получить информацию по API", func(t *testing.T) {
+		data, err := zc.GetAPIInfo(t.Context())
+		assert.NoError(t, err)
+
+		log.Println("Zabbix API informetion:", string(data))
+	})
+
+	t.Run("Тест 2. Обновить информацию по группе хостов", func(t *testing.T) {
+
+	})
+
+	t.Run("Тест 3. Обновить информацию по хостам", func(t *testing.T) {
+
+	})
+
+	t.Cleanup(func() {
+		os.Unsetenv("GO_TESTZABBIX_HOST")
+		os.Unsetenv("GO_TESTZABBIX_PORT")
+		os.Unsetenv("GO_TESTZABBIX_USER")
+		os.Unsetenv("GO_TESTZABBIX_PASSWD")
+	})
+}
